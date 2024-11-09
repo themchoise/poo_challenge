4e12201b176bfa9d8057bb201bf3dc46a0184c66
Maximiliano Joyce Sp Team <140093681+ttinydeveloper@users.noreply.github.com>
2024-11-09 14:36:20 -0300

    inicio

--- markerian-coscarelli-mele-santiago-joyce/app/importar_csv.py
+++ markerian-coscarelli-mele-santiago-joyce/app/importar_csv.py
@@ -1,9 +1,11 @@
 import pandas as pd
 from pandas import DataFrame
-def importar_datos(path:str) -> DataFrame | bool:
 
+
+def importar_datos(path: str) :
     try:
-          return pd.read_csv(path, sep=";",  index_col=0, encoding='latin-1')
+        return pd.read_csv(path, sep=";", index_col=0, encoding='latin-1')
+
     except FileNotFoundError as e:
         print("Error al conectar con el dataset.")
         return False
